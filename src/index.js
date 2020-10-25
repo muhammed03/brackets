da812a2eb2abfe34508fd07993066b5843e79663
abdulhakim <hakim@abdulhakims-MacBook-Pro.local>
2020-10-25 15:36:09 +0600

    feat: updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.split('').length%2!=0){
+    console.log("false")
+    return false;
+  }
+  let Config = {};
+
+  for (let i=0; i<bracketsConfig.length;i++){
+    Config[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  let array = str.split('');
+  let stack = [];
+  let n = 0;
+  for (let i = 0; i<=array.length;i++){
+
+    if (i===array.length){
+      if (stack.length!=0){
+          return false;
+      }
+      else {
+          return true;
+      }
+    }
+    if (array[i]===stack[0]){
+      stack.shift();
+    }
+    else if (array[i] in Config){
+        stack.unshift(Config[array[i]]);
+    }
+  }
+  return true;
 }
